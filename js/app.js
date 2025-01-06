e31da6671ffe51e9b1bbd8772ad83605baf03e89
Lisa H. <neuroticNanobot@outlook.com>
2025-01-05 22:41:03 -0500

    JS implementation done

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-"using strict";
+"use strict";
 
 let count = 0;
 
@@ -7,55 +7,42 @@ const incrementBtn = document.querySelector(".increment-btn");
 const resetBtn = document.querySelector(".reset-btn");
 const decrementBtn = document.querySelector(".decrement-btn");
 
-
 function incrementCount() {
-    // Write the relevant code in this block
-
+  // Write the relevant code in this block
+  count++;
 }
-
 
 function decrementCount() {
-    // Write the relevant code in this block
-
+  // Write the relevant code in this block
+  count--;
 }
 
-
 function resetCount() {
-    // Write the relevant code in this block
-    
+  // Write the relevant code in this block
+  count = 0;
 }
 
 function renderUpdatedCount() {
-    countElement.innerText = count;
+  countElement.innerText = count;
 }
 
 incrementBtn.addEventListener("click", function () {
-    // Write code below this line
-
-
-
-    // STOP HERE
-
-
-    renderUpdatedCount();
+  // Write code below this line
+  incrementCount();
+  // STOP HERE
+  renderUpdatedCount();
 });
 
 resetBtn.addEventListener("click", function () {
-    // Write code below this line
-    
-
-    // STOP HERE
-    
-    
-    renderUpdatedCount();
+  // Write code below this line
+  resetCount();
+  // STOP HERE
+  renderUpdatedCount();
 });
 
 decrementBtn.addEventListener("click", function () {
-    // Write code below this line
-
-
-    // STOP HERE
-
-
-    renderUpdatedCount();
+  // Write code below this line
+  decrementCount();
+  // STOP HERE
+  renderUpdatedCount();
 });
